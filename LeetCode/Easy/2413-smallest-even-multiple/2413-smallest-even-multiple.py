944e8075578dc3386126771c71c95275c987aa3f
MinkyungPark <park29@kakao.com>
2026-06-19 13:10:24 +0900

    delete problem

--- LeetCode/Easy/2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def smallestEvenMultiple(self, n: int) -> int:
-        a, b = 2, n
-        
-        while b != 0:
-            a, b = b, a % b
-        gcd = a
-
-        return n // gcd * 2
