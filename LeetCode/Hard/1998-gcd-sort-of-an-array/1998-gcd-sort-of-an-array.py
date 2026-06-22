54325f4033d49a4b49b4a0b8ce437d0e11995edf
박민경 <parkminkyung32@gmail.com>
2026-06-22 11:54:16 +0900

    Array 647 ms (76.34%) 27.2 MB (74.19%)

--- LeetCode/Hard/1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.py
+++ LeetCode/Hard/1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.py
@@ -1,7 +1,64 @@
 # can swap if gcd(nums[i], nums[j]) > 1
 # union among prime factors
 
+# --------------- Linear seive for O(m + n log n) --------------- #
 class Solution:
+    def gcdSort(self, nums: List[int]) -> bool:
+        max_num = max(nums)
+        parent = list(range(max_num + 1))
+
+        def find(a):
+            while parent[a] != a:
+                parent[a] = parent[parent[a]]
+                a = parent[a]
+            return a
+ 
+        def union(a, b):
+            root_a = find(a)
+            root_b = find(b)
+
+            if root_a != root_b:
+                parent[root_b] = root_a
+
+        spf = [0] * (max_num + 1)
+        primes = []
+
+        for i in range(2, max_num + 1):
+            if spf[i] == 0:
+                spf[i] = i
+                primes.append(i)
+
+            for prime in primes:
+                val = prime * i
+                
+                if val > max_num:
+                    break
+                
+                spf[val] = prime
+
+                if prime == spf[i]:
+                    break
+        
+        for num in nums:
+            x = num
+
+            while x > 1:
+                factor = spf[x]
+                union(num, factor)
+
+                while x % factor == 0:
+                    x //= factor
+        
+        sorted_nums = sorted(nums)
+
+        for a, b in zip(sorted_nums, nums):
+            if find(a) != find(b):
+                return False
+    
+        return True
+
+# --------------- O(m log log m + n log n) --------------- #
+class Solution2:
     def gcdSort(self, nums: List[int]) -> bool:
         max_num = max(nums)
         parent = list(range(max_num + 1))
