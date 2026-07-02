869158f4275de449191298946d2a1de22b1e5d68
박민경 <parkminkyung32@gmail.com>
2026-07-02 14:42:25 +0900

    Array 1440 ms (36.11%) 23.9 MB (99.66%)

---
+++ LeetCode/Medium/0204-count-primes/0204-count-primes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+
+        # Eratosthenes'sieve 
+        # log log 5,000,000 ≈ log(15.42) ≈ 2.73
+        # n log log n = 5,000,000 * 2.73 ≈ 13,650,000
+        
+        is_prime = bytearray(b'\x01') * n
+        is_prime[0] = 0
+        is_prime[1] = 0
+
+        for i in range(2, int(n ** 0.5) + 1):
+            if is_prime[i]:
+                for multiple in range(i * i, n, i):
+                    is_prime[multiple] = 0
+
+        return sum(is_prime)
+        
+        return sum(is_prime)
