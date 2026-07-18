1fdb33203362ce9f2c557756c294b44f57c2b9b1
박민경 <parkminkyung32@gmail.com>
2026-07-18 23:02:39 +0900

    Array 230 ms (91.02%) 21.6 MB (72.05%)

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
@@ -2,30 +2,24 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n, m = len(grid), len(grid[0])
+        r, c = len(grid), len(grid[0])
 
-        def bfs(x, y):
-            q = deque([(x, y)])
+        def dfs(i, j):
+            if i < 0 or i >= r or j < 0 or j >= c or grid[i][j] != '1':
+                return
 
-            while q:
-                x, y = q.popleft()
-
-                for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                    nx, ny = x + dx, y + dy
-                    if (
-                        0 <= nx < n and 0 <= ny < m
-                        and grid[nx][ny] == "1"
-                    ):
-                        grid[nx][ny] = "0"
-                        q.append((nx, ny))
+            grid[i][j] = '0'
+            
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
         
         ans = 0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == "0":
-                    continue
-
-                bfs(i, j)
-                ans += 1
-
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    ans += 1
+                    dfs(i, j)
+        
         return ans
