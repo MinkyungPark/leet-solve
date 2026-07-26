6fc8e74441f2cb21f5e12e38e5e1d239b1263183
박민경 <parkminkyung32@gmail.com>
2026-07-26 22:16:46 +0900

    Array 905 ms (36.32%) 19.4 MB (93.03%)

---
+++ LeetCode/Hard/1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.py
@@ -0,0 +1,48 @@
+"""
+0 : no land
+1 : one island
+2 : already seperated
+"""
+
+class Solution:
+    def minDays(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def count_islands():
+            visited = set()
+
+            def dfs(x, y):
+                stack = [(x, y)]
+                while stack:
+                    x, y = stack.pop()
+                    for nx, ny in [(x-1, y), (x+1, y), (x, y+1), (x, y-1)]:
+                        if (
+                            0 <= nx < m and 0 <= ny < n
+                            and grid[nx][ny] == 1
+                            and (nx, ny) not in visited
+                        ):
+                            visited.add((nx, ny))
+                            stack.append((nx, ny))
+            
+            islands = 0
+            for i in range(m):
+                for j in range(n):
+                    if grid[i][j] and (i, j) not in visited:
+                        islands += 1
+                        visited.add((i, j))
+                        dfs(i, j)
+            
+            return islands
+        
+        if count_islands() != 1:
+            return 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    grid[i][j] = 0
+                    if count_islands() != 1: # Still one island after removing one land cell
+                        return 1
+                    grid[i][j] = 1
+        
+        return 2
